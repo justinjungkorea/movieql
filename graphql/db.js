8ef3e02d0589a5241bbd3770d153a6f75abe682e
Justin_Jung <jungdw0624@gmail.com>
2019-03-25 17:39:01 +0900

    update

--- graphql/db.js
+++ graphql/db.js
@@ -2,32 +2,32 @@ let movies = [
     {
         id:0,
         name:"Good Will Hunting",
-        score:4.9
+        score:49
     },
     {
         id: 1,
         name: "National Treasure",
-        score: 4.0
+        score: 40
     },
     {
         id: 2,
         name: "The Godfather",
-        score: 4.8
+        score: 48
     },
     {
         id: 3,
         name: "Ocean's eleven",
-        score: 4.7
+        score: 47
     },
     {
         id: 4,
         name: "Ocean's twelve",
-        score: 4.2
+        score: 42
     },
     {
         id: 5,
         name: "Ocean's thirteen",
-        score: 4.1
+        score: 41
     }
 ];    
 
@@ -48,3 +48,13 @@ export const deleteMovie = id => {
         return false;
     }
 }
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: `${movies.lenth + 1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
+};
